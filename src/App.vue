34540f0ef4962460149cf1bc66fc9bb2a873665c
xant3r0 <scandiba13@gmail.com>
2023-06-23 14:40:54 +0300

    Fixed some bugs

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
 export default {
   data() {
     return {
-    display:'',
+    display:'0',
     secondValue:"",
     v1:"",
     v2:"",
@@ -41,89 +41,129 @@ export default {
   methods: {
     addOnDisplay1(event) {
       event.preventDefault();
-      this.display = this.display + 1;
+      if(this.display !== "0") {
+        this.display = this.display + 1;
+      } else {
+        this.display = "1";
+      }
     },
     addOnDisplay2(event) {
       event.preventDefault();
-      this.display = this.display + 2;
+      if(this.display !== "0") {
+        this.display = this.display + 2;
+      } else {
+        this.display = "2";
+      }
     },
     addOnDisplay3(event) {
       event.preventDefault();
-      this.display = this.display + 3;
+      if(this.display !== "0") {
+        this.display = this.display + 3;
+      } else {
+        this.display = "3";
+      }
     },
     addOnDisplay4(event) {
       event.preventDefault();
-      this.display = this.display + 4;
+      if(this.display !== "0") {
+        this.display = this.display + 4;
+      } else {
+        this.display = "4";
+      }
     },
     addOnDisplay5(event) {
       event.preventDefault();
-      this.display = this.display + 5;
+      if(this.display !== "0") {
+        this.display = this.display + 5;
+      } else {
+        this.display = "5";
+      }
     },
     addOnDisplay6(event) {
       event.preventDefault();
-      this.display = this.display + 6;
+      if(this.display !== "0") {
+        this.display = this.display + 6;
+      } else {
+        this.display = "6";
+      }
     },
     addOnDisplay7(event) {
       event.preventDefault();
-      this.display = this.display + 7;
+      if(this.display !== "0") {
+        this.display = this.display + 7;
+      } else {
+        this.display = "7";
+      }
     },
     addOnDisplay8(event) {
       event.preventDefault();
-      this.display = this.display + 8;
+      if(this.display !== "0") {
+        this.display = this.display + 8;
+      } else {
+        this.display = "8";
+      }
     },
     addOnDisplay9(event) {
       event.preventDefault();
-      this.display = this.display + 9;
+      if(this.display !== "0") {
+        this.display = this.display + 9
+      } else {
+        this.display = "9";
+      }
     },
     addOnDisplay0(event) {
       event.preventDefault();
-      this.display = this.display + 0;
+      if(this.display !== "0") {
+        this.display = this.display + 0;
+      } else {
+        this.display = "0";
+      }
     },
 
     //II cacat,das nub ,csf
     deleteFromScreen(event) {
       event.preventDefault();
-      this.display = '';
+      this.display = '0';
     },
     addNumbers(event) {
       event.preventDefault();
       this.plus++;
-      if(this.display === '') {
+      if(this.display === '0') {
         this.secondValue = 0;
       } else {
         this.secondValue = this.display;
       }
-      this.display = '';
+      this.display = '0';
     },
     downNumbers(event) {
       event.preventDefault();
       this.minus++;
-      if(this.display === '') {
+      if(this.display === '0') {
         this.secondValue = 0;
       } else {
         this.secondValue = this.display;
       }
-      this.display = '';
+      this.display = '0';
     },
     multNumbers(event) {
       event.preventDefault();
       this.plus = -1;
-      if(this.display === '') {
+      if(this.display === '0') {
         this.secondValue = 0;
       } else {
         this.secondValue = this.display;
       }
-      this.display = '';
+      this.display = '0';
     },
     divNumbers(event) {
       event.preventDefault();
       this.plus = -2;
-      if(this.display === '') {
+      if(this.display === '0') {
         this.secondValue = 0;
       } else {
         this.secondValue = this.display;
       }
-      this.display = '';
+      this.display = '0';
     },
     Equal(event) {
       event.preventDefault();
@@ -134,6 +174,9 @@ export default {
           this.display = this.v2 + this.v1;
           this.plus = 0;
           this.minus = 0;
+          this.secondValue = '0';
+          this.v1 = '0';
+          this.v2 = '0';
           break;
         case 0:
           this.v1 = parseFloat(this.display);
@@ -141,6 +184,9 @@ export default {
           this.display = this.v2 - this.v1;
           this.plus = 0;
           this.minus = 0;
+          this.secondValue = '0';
+          this.v1 = '0';
+          this.v2 = '0';
           break;
         case -1:
           this.v1 = parseFloat(this.display);
@@ -148,6 +194,9 @@ export default {
           this.display = this.v2 * this.v1;
           this.plus = 0;
           this.minus = 0;
+          this.secondValue = '0';
+          this.v1 = '0';
+          this.v2 = '0';
           break;
         case - 2:
           this.v1 = parseFloat(this.display);
@@ -155,6 +204,9 @@ export default {
           this.display = this.v2 / this.v1;
           this.plus = 0;
           this.minus = 0;
+          this.secondValue = '0';
+          this.v1 = '0';
+          this.v2 = '0';
           break;
       }
     },
@@ -165,7 +217,9 @@ export default {
     },
     addComma(event) {
       event.preventDefault();
-      this.display = this.display + ".";
+      if(this.display[this.display.length - 1] !== '.') {
+        this.display = this.display + '.';
+      }
     }
   }
 }
